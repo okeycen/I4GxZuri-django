31fd2e339aad2c73d419d814f3dc3dd93063a3d3
okeycen <okeycen2m@gmail.com>
2022-06-04 01:25:39 +0100

    My first django project

--- nnaegbo_proj/nnaegbo_proj/settings.py
+++ nnaegbo_proj/nnaegbo_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!@u5gf-^yx(b2a369%t=ysn%n19_5_&=j63*82^#qnar_g=zck'
+SECRET_KEY = 'django-insecure-v$o8(tr-+3!##(cs&u647-l7gx6#^uc15-6-a!-xiaxl_vl@^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
